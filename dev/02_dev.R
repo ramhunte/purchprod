e9580cb641f2758c7da624c3f1cb18dc90c91166
ramhunte <ramhunte@ucsb.edu>
2025-04-30 15:32:19 -0700

    solving markdown issue

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -30,6 +30,7 @@ usethis::use_package("bsicons")
 usethis::use_package("utils")
 usethis::use_package("stats")
 
+
 ## Add modules ----
 ## Create a module infrastructure in R/
 golem::add_module(name = "name_of_module1", with_test = TRUE) # Name of the module
